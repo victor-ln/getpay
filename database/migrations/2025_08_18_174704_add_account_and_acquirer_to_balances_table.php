fdbf24c509c44ed35672fadca69dbc0d0f6d97a7
Luciano Vargas <lucianovargas88@hotmail.com>
2025-08-24 22:40:00 -0300

    production

--- database/migrations/2025_08_18_174704_add_account_and_acquirer_to_balances_table.php
+++ database/migrations/2025_08_18_174704_add_account_and_acquirer_to_balances_table.php
@@ -19,20 +19,6 @@ return new class extends Migration
                 ->after('id')
                 ->constrained('accounts')
                 ->onDelete('set null');
-
-            // Adiciona um índice na coluna 'status'.
-            // Isso acelera buscas por pagamentos com um status específico (ex: 'pending', 'paid').
-            $table->index('status');
-
-            // Adiciona um índice em um possível ID de transação externa.
-            // Essencial para localizar rapidamente um pagamento a partir do ID de um gateway.
-            $table->index('external_payment_id');
-            $table->index('provider_transaction_id');
-
-            // Adiciona um índice composto.
-            // Otimiza consultas que filtram por status e ordenam por data,
-            // um caso de uso muito comum para exibir transações recentes.
-            $table->index(['status', 'created_at']);
         });
     }
 
