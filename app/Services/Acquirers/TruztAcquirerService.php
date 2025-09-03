1185cc2b69fc6b866ff85a0b476ea6a004adc28f
luciano <lucianovargas88@hotmail.com>
2025-09-03 17:34:27 -0300

    webhook

--- app/Services/Acquirers/TruztAcquirerService.php
+++ app/Services/Acquirers/TruztAcquirerService.php
@@ -91,7 +91,7 @@ class TruztAcquirerService implements AcquirerInterface
                     'amount' => (float) $data['amount'],
                     'username' => $this->username,
                     'password' => $this->password,
-                    'webhook' => 'https://fdendpoint-getpay-prod-3-gkhpfde9fveub8bb.z01.azurefd.net/api/webhook/handler'
+                    'webhook' => 'https://hub.getpay.one/api/webhook/handler'
 
                 ]);
 
@@ -142,7 +142,7 @@ class TruztAcquirerService implements AcquirerInterface
                     'username' => $this->username,
                     'password' => $this->password,
                     'amount' => (float) $data['amount'],
-                    'webhook' => 'https://fdendpoint-getpay-prod-3-gkhpfde9fveub8bb.z01.azurefd.net/api/webhook/handler',
+                    'webhook' => 'https://hub.getpay.one/api/webhook/handler',
                     'document' => $data['documentNumber'],
                     'pixKey' => $data['pixKey'],
                     'pixKeyType' => $data['pixKeyType'],
