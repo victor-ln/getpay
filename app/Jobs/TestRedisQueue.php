3c29c6c755910c9515a320216b0e09f9545c6b45
Victor Nunes <vlima-nu@student.42sp.org.br>
2025-10-06 22:06:29 -0300

    feat: Queue Connection with Redis

---
+++ app/Jobs/TestRedisQueue.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log; // Importe a classe Log
+
+class TestRedisQueue implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(): void
+    {
+        // A linha que vamos procurar no log
+        Log::info('🎉 O Job TestRedisQueue foi executado com sucesso pela fila!');
+    }
+}
